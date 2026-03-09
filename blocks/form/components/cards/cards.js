626c78691a7e004661742ea173513c807f5e451b
Nitesh Kumar <niteshk@Niteshs-MacBook-Pro.local>
2026-03-09 21:23:46 +0100

    added custom component

--- blocks/form/components/cards/cards.js
+++ blocks/form/components/cards/cards.js
@@ -24,9 +24,12 @@ export default async function decorate(fieldDiv, fieldJson, parentElement, formI
       const { payload } = e;
 
       payload?.changes?.forEach((change) => {
-       // if (change?.currentValue === 'enum') {
-          console.log('🔄 Enum changed:', change.currentValue);
-        //}
+         console.log('🔄 Enum changed:', change.currentValue);
+         if (change.currentValue === '0') {
+          console.log('🔄 Item 1');
+         } else if (change.currentValue === '1') {
+          console.log('🔄 Item 2');
+         }
       });
     });
   });
