8cb4bf640046e4410820c3e64d9657e5d09c0e74
Nitesh Kumar <niteshk@Niteshs-MacBook-Pro.local>
2026-03-18 21:25:00 +0100

    add custom comp

--- blocks/form/components/cards/cards.js
+++ blocks/form/components/cards/cards.js
@@ -1,6 +1,9 @@
 import { createOptimizedPicture } from '../../../../scripts/aem.js';
 import { subscribe } from '../../rules/index.js';
 
+/** Default card art per option index (first = red, second = green). Override via enum item `image`. */
+const DEFAULT_CARD_IMAGES = ['/icons/redcard.png', '/icons/greencard.png'];
+
 function createCard(element, enums) {
   element.querySelectorAll('.radio-wrapper').forEach((radioWrapper, index) => {
     if (enums[index]?.name) {
@@ -17,10 +20,12 @@ function createCard(element, enums) {
     // Attach index to input element for later reference
     radioWrapper.querySelector('input').dataset.index = index;
 
-    const image = createOptimizedPicture(
-      enums[index]?.image || 'https://customcomp--formtrooper--curiousnk.aem.page/icons/redcard.png',
-      'card-image',
-    );
+    const imageSrc =
+      enums[index]?.image
+      || DEFAULT_CARD_IMAGES[index % DEFAULT_CARD_IMAGES.length]
+      || DEFAULT_CARD_IMAGES[0];
+
+    const image = createOptimizedPicture(imageSrc, 'card-image');
 
     radioWrapper.appendChild(image);
   });
